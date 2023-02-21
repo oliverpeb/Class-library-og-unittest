6315b50ae5e6f2b2715718ae4680888ef314c999
oliverpeb <83925702+oliverpeb@users.noreply.github.com>
2023-02-21 13:48:12 +0100

    Add project files.

---
+++ Class library og unittest/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace Class_library_og_unittest
+    
+{
+    public class Car
+    {
+        //Test
+        public int id { get; set; }
+        public string model { get; set; }
+        public int price { get; set; } 
+        public string licenseplate { get; set; }
+
+        public void Validateid()
+        {
+            if (id == null)
+                throw new ArgumentException("id cannot be null");
+        }
+
+        public void ValidateModel()
+        {
+            
+            if (model.Length < 4)
+                throw new ArgumentException("The model name cannot be less than 4");
+
+        }
+
+        public void ValidatePrice()
+        {
+            if (price < 0)
+                throw new ArgumentOutOfRangeException("the price of the car cannot be less than 1" + price);
+        }
+
+        public void ValidateLicensePlate()
+        {
+            if (licenseplate.Length <= 2)
+                throw new ArgumentOutOfRangeException("The licenseplate value must be between 2 and 7");
+            if (licenseplate.Length >= 7)
+                throw new ArgumentOutOfRangeException("The licenseplate value must be between 2 and 7");
+        }
+
+        public void Validate ()
+        {
+            Validateid();
+            ValidateModel();
+            ValidatePrice();
+            ValidateLicensePlate();
+        }
+    }
+}
---
+++ Class library og unittestTests/CarTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Class_library_og_unittest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using Class_library_og_unittest;
+
+namespace Class_library_og_unittest.Tests
+{
+    [TestClass()]
+    public class CarTests
+    {
+        private Car car = new Car { id = 5, model = "123456", price = 4, licenseplate = "12345" };
+        private Car carmodelnameshort = new Car { id = 1, model = "123", price = 0, licenseplate = "12345" };
+        private Car carpriceislessthanzero = new Car { id = 2, model = "12345", price = -5, licenseplate = "12345" };
+        private Car carlicenseplateistooshort = new Car { id = 3, model = "12345", price = 2, licenseplate = "12" };
+
+
+
+        [TestMethod()]
+        public void ValidateModelTest()
+        {
+            car.ValidateModel();
+            Assert.ThrowsException<ArgumentException>(() => carmodelnameshort.ValidateModel());
+        }
+        [TestMethod()]
+        public void ValidatePriceTest()
+        {
+            car.ValidatePrice();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carpriceislessthanzero.ValidatePrice());
+        }
+        [TestMethod()]
+        public void ValidateLicensePlateTest()
+        {
+            car.ValidateLicensePlate();
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => carlicenseplateistooshort.ValidateLicensePlate());
+        }
+
+
+    }
+}
